eab04ee2dcffa6c13e92d4c83a183021bb96882c
MinhtheHome <minhthe.007@gmail.com>
2016-07-06 04:31:50 +0700

    Initial commit - same with tutorial

---
+++ app/src/main/java/com/example/dell/simplelauncher/AppsListActivity.java
@@ -0,0 +1,15 @@
+package com.example.dell.simplelauncher;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AppsListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+    }
+
+}
